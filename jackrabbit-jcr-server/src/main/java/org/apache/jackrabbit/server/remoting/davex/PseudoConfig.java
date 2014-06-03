9a59756cf963228ca101662ae2b7eab25df30230
Alfusainey <alf.jallow@gmail.com>
2014-06-03 13:10:58 +0200

    configure p-i-importers

--- jackrabbit-jcr-server/src/main/java/org/apache/jackrabbit/server/remoting/davex/PseudoConfig.java
+++ jackrabbit-jcr-server/src/main/java/org/apache/jackrabbit/server/remoting/davex/PseudoConfig.java
@@ -10,31 +10,20 @@ import org.apache.jackrabbit.core.config.ImportConfig;
 import org.apache.jackrabbit.core.xml.AccessControlImporter;
 import org.apache.jackrabbit.core.xml.ProtectedNodeImporter;
 
-/*
- * Factory for creating an importer and plugging-in a custom pseudo configuration
- * containing an "access control import handler".
- *
- */
-public class ImporterFactory {
+final class PseudoConfig extends ImportConfig {
+
+    private final ProtectedNodeImporter aci;
+
+    PseudoConfig() {
+        this.aci = new AccessControlImporter();    
+    }
 
     public ImportConfig getWorkspaceConfig() {
         return new PseudoConfig();
     }
 
-    // --------------------------------- ImportConfig
-	
-    private final class PseudoConfig extends ImportConfig {
-		
-        private final ProtectedNodeImporter aci;
-		
-        private PseudoConfig() {
-            this.aci = new AccessControlImporter();		
-        }
-		
-        @Override		
-        public List<? extends ProtectedNodeImporter> getProtectedItemImporters() {
-            return Collections.singletonList(aci);
-        }
-    }
-
+    @Override
+    public List<? extends ProtectedNodeImporter> getProtectedItemImporters() {
+        return Collections.singletonList(aci);   
+    }   
 }
